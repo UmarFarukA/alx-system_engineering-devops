e49cff474644d773b88235bb52f834c7a5c24b81
UmarFarukA <talk2ufaz@gmail.com>
2023-03-24 06:21:32 -0700

    change:path

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@ file { '/school':
   ensure  => 'present',
   content => 'I love Puppet',
   group   => 'www-data',
-  mode    => '0744'
+  mode    => '0744',
   owner   => 'www-data',
   path    => '/tmp/school',
 }
