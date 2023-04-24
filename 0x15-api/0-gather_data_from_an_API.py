9f65d429cbf58c80e26e05136191364a48350f18
UmarFarukA <talk2ufaz@gmail.com>
2023-04-24 07:16:44 -0700

    initial commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     todosUrl = url + "/todos"
     todos = requests.get(todosUrl, params={"userId": sys.argv[1]}).json()
     user = requests.get(userUrl).json()
-    completed = [todo for todo in todos if todo['completed'] is True]
+    completed = [todo['title'] for todo in todos if todo['completed'] is True]
     print("Employee {} is done with tasks({}/{}):"
           .format(user.get('name'), len(completed), len(todos)))
     [print("\t {}".format(todo['title']))
