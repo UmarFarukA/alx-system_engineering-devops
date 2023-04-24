57cd06eeff444f71658e4f189747d9332a0ec509
UmarFarukA <talk2ufaz@gmail.com>
2023-04-24 10:33:24 -0700

    added:loop condition

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""A script that load data to csv file"""
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
-import sys
-
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users").json()
-    with open("todo_all_employees.json", "w") as fw:
-        for userId in range(1, len(user)):
-            user = requests.get(url + "users/{}".format(userId)).json()
-            todos = requests.get(url + "todos",
-                                 params={"userId": userId}).json()
-            username = user.get("username")
-            json.dump({userId: [{"task": todo.get("title"),
-                                 "completed": todo.get("completed"),
-                                 "username": userName} for todo in todos]}, fw)
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
