90dea521b5182b80e88f19ffdd21c4361f504ecd
UmarFarukA <talk2ufaz@gmail.com>
2023-04-24 06:39:16 -0700

    initial commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,21 +4,21 @@ import requests
 import sys
 
 
-url = "https://jsonplaceholder.typicode.com"
-user = url + "/users/{}".format(sys.argv[1])
-todos = url + "/todos?userId={}".format(sys.argv[1])
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    user = url + "/users/{}".format(sys.argv[1])
+    todos = url + "/todos?userId={}".format(sys.argv[1])
+    response = requests.get(todos)
+    res = requests.get(user)
 
-response = requests.get(todos)
-res = requests.get(user)
+    tasks = response.json()
+    theUser = res.json()
 
-tasks = response.json()
-theUser = res.json()
-
-completed = 0
-for task in tasks:
-    if task['completed']:
-        completed += 1
-print("Employee {} is done with task ({}/{}):"
-      .format(theUser.get('name'), completed, len(tasks)))
-[print("\t {}".format(task['title']))
- for task in tasks if task['complete']]
+    completed = 0
+    for task in tasks:
+        if task['completed']:
+            completed += 1
+    print("Employee {} is done with task ({}/{}):"
+          .format(theUser.get('name'), completed, len(tasks)))
+    [print("\t {}".format(task['title']))
+     for task in tasks if task['complete']]
