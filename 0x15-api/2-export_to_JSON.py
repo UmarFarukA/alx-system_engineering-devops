d3745c09ce26d30620e9b0b9a1a6187158e71155
UmarFarukA <talk2ufaz@gmail.com>
2023-04-24 10:06:22 -0700

    initial commit

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A script that load data to csv file"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    userId = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(userId)).json()
+    userName = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": userId}).json()
+
+    with open("{}.json".format(user_id), "w") as fw:
+        json.dump({userId: [{"task": todo.get("title"),
+                             "completed": todo.get("completed"),
+                             "username": userName} for todo in todos]}, fw)
